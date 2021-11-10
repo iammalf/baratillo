e03d26e58986728a276bab0b2434bb91eee03312
Lorem <lorem@lorem.comm>
2021-11-09 23:52:02 -0500

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+      <footer>footer</footer>
+    </div>
+  );
+}
